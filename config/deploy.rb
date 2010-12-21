9ea652b62c31b7f34d71a08b00e84a09fadfc488
Olivier Kouame <olivier.kouame@gmail.com>
2010-12-21 15:03:31 +0000

    Configuration changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,13 +6,13 @@ set :application, "geocms"
 set :repository,  "git@github.com:okouam/geocms.git"
 set :scm, :git
 set :branch, :master
-set :stages, ["uat", "production"]
+set :stages, ["staging", "production"]
 
-set :default_stage, "uat"
+set :default_stage, "staging"
 set :deploy_via, :remote_cache
 set :user, "okouam"
 set :ssh_options, { :forward_agent => true }
-set :deploy_to, "/home/opt/Rails/apps/geocms/uat"
+set :deploy_to, "/home/opt/Rails/apps/geocms/staging"
 set :rake, "/var/lib/gems/1.8/bin/rake"
 role :web, "xkcd.codeifier.com"
 role :app, "xkcd.codeifier.com"
