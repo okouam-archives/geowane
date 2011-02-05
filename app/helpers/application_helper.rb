d0dcce138b3ce6127648a37b6b6aa36e26cad031
okouam <olivier.kouame@gmail.com>
2011-02-05 23:28:50 +0000

    Added views for statistics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def linked_figure(figure, url)
     figure = 0 unless figure
-    if figure > 0 && url
+    if figure.to_i > 0 && url
       "<a href=\"#{url}\">#{number_with_delimiter(figure, :separator => ",")}</a>".html_safe
     else
       number_with_delimiter(figure).html_safe
