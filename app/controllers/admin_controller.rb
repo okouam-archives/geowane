59816336fce8b9db4079dde004ab7d15b5d48ca2
okouam <olivier.kouame@gmail.com>
2011-02-23 23:03:42 +0000

    Added additional counters to dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,8 @@ class AdminController < ApplicationController
 
   def dashboard
     @location_count = Location.count
+    @tag_count = Tag.count
+    @untagged_count = @location_count - ActiveRecord::Base.connection.execute("SELECT count(distinct locations.id) as \"total\" FROM locations JOIN tags on tags.location_id = locations.id")[0]["total"].to_i
     @level_3_count = AdministrativeUnit.where("level = 3").count
     @level_1_count = AdministrativeUnit.where("level = 1").count
     @level_2_count = AdministrativeUnit.where("level = 2").count
