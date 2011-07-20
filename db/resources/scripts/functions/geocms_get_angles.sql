06d65361e631759f508d49ee36e9501ec2cba935
okouam <olivier.kouame@gmail.com>
2011-07-20 17:49:07 +0100

    All

---
+++ db/resources/scripts/functions/geocms_get_angles.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION geocms_get_angles(IN segment geometry)
+  RETURNS TABLE(start_azimut double precision, end_azimut double precision) AS
+$BODY$
+  DECLARE
+    num_points integer;
+    start_points geometry[];
+    end_points geometry[];
+    start_angle double precision;
+    end_angle double precision;
+  BEGIN
+
+    num_points = ST_NumPoints(segment);
+
+    start_points = ARRAY[ST_PointN(segment, 1), ST_PointN(segment, 2)];
+    start_angle = ST_Azimuth(start_points[1], start_points[2]) / (2*pi()) * 360;
+
+    end_points = ARRAY[ST_PointN(segment, num_points - 1), ST_PointN(segment, num_points)];
+    end_angle = ST_Azimuth(end_points[1], end_points[2]) / (2*pi()) * 360;
+
+    RETURN QUERY SELECT start_angle, end_angle;
+
+  END;
+  $BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100
+  ROWS 1000;
--- db/resources/scripts/functions/routing.sql
+++ db/resources/scripts/functions/routing.sql
@@ -101,25 +101,3 @@ $BODY$
   END;
 $BODY$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION geocms_create_connector(road geometry, label character varying(255), next_road geometry, route geometry[], new_point geometry)
-RETURNS TABLE(a text, b character varying, d double precision, e double precision) AS
-$BODY$
-  DECLARE
-  fraction float;
-  left geometry;
-  right geometry;
-  start_angle float;
-  end_angle float;
-  BEGIN
-    fraction = ST_Line_Locate_point(road, new_point);
-    left = ST_Line_Substring(road, 0, fraction);
-    right = ST_Line_Substring(road, fraction, 1);
-    IF ST_Touches(next_road, left) AND ST_GeometryType(left) = 'ST_LineString' THEN
-      SELECT * INTO start_angle, end_angle FROM geocms_get_angles(left);
-      RETURN QUERY SELECT ST_AsText(left), label, start_angle, end_angle;
-    ELSIF ST_GeometryType(right) = 'ST_LineString' THEN
-      SELECT * INTO start_angle, end_angle FROM geocms_get_angles(right);
-      RETURN QUERY SELECT ST_AsText(right), label, start_angle, end_angle;
-    END IF;
-  END;
-$BODY$ LANGUAGE plpgsql;
