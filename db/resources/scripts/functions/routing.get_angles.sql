fdd2f6a22f45a29ca19791d2f01b50405ab5bad2
okouam <olivier.kouame@gmail.com>
2011-07-26 23:15:37 +0100

    Moved routing functions to routing schema and reports to reports schema

--- db/resources/scripts/functions/routing.get_angles.sql
+++ db/resources/scripts/functions/routing.get_angles.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION geocms_get_angles(IN segment geometry)
+CREATE OR REPLACE FUNCTION routing.get_angles(IN segment geometry)
   RETURNS TABLE(start_azimut double precision, end_azimut double precision) AS
 $BODY$
   DECLARE
