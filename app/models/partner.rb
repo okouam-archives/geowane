34e46143748daaf4bed13ed3b0d725af12b2f50c
okouam <olivier.kouame@gmail.com>
2011-08-13 13:43:37 +0100

    Fixed bug for partners dropdown list

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -2,7 +2,7 @@ class Partner < ActiveRecord::Base
   has_many :classifications
 
   def self.dropdown_items
-    Partner.all.order("name ASC").map {|partner| [partner.name, partner.id]}
+    Partner.order("name ASC").map {|partner| [partner.name, partner.id]}
   end
 
 end
