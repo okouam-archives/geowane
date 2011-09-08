0c4b102766ca47ae0a77935a125d27ccadbd2a03
okouam <olivier.kouame@gmail.com>
2011-09-08 11:11:57 +0100

    ALl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Gowane::Application.configure do
   config.action_controller.perform_caching = true
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  #config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
@@ -29,7 +29,7 @@ Gowane::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -43,7 +43,8 @@ Gowane::Application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
+  config.assets.compress = true
+  config.assets.js_compressor = :uglifier
 
-  # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 end
