2d5895f88e5efb8e219c7a0fbd236c1041148abd
docteurdiam <diameramoussa@yahoo.fr>
2011-09-21 18:05:55 +0000

    logo upload

---
+++ db/migrate/20110921101713_create_logos.rb
@@ -0,0 +1,13 @@
+class CreateLogos < ActiveRecord::Migration
+
+  def up
+    create_table :logos do |t|
+      t.string :image
+      t.timestamps
+      t.references :location
+    end
+  end
+
+  def down
+  end
+end
