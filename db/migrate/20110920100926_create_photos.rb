267e8c4dd9568db8e7f3d5a3e57435a088123cb9
okouam <olivier.kouame@gmail.com>
2011-10-04 23:01:25 +0100

    Fixed migrations

--- db/migrate/20110920100926_create_photos.rb
+++ db/migrate/20110920100926_create_photos.rb
@@ -5,7 +5,7 @@ class CreatePhotos < ActiveRecord::Migration
       CREATE TABLE photos
       (
         id serial,
-        location_id integer REFERENCES locations(id)
+        location_id integer REFERENCES locations(id),
         image character varying(255),
         created_at timestamp without time zone,
         updated_at timestamp without time zone,
--- db/migrate/20110927100228_create_logos.rb
+++ db/migrate/20110927100228_create_logos.rb
@@ -5,7 +5,7 @@ class CreateLogos < ActiveRecord::Migration
       CREATE TABLE logos
       (
         id serial,
-        location_id integer REFERENCES locations(id)
+        location_id integer REFERENCES locations(id),
         image character varying(255),
         created_at timestamp without time zone,
         updated_at timestamp without time zone,
