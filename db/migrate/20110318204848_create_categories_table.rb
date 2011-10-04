ac3c43da4fc2f15515239ba00b8d7f11cb5fa360
okouam <olivier.kouame@gmail.com>
2011-10-04 22:55:23 +0100

    Fixed migrations

--- db/migrate/20110318204848_create_categories_table.rb
+++ db/migrate/20110318204848_create_categories_table.rb
@@ -9,14 +9,12 @@ class CreateCategoriesTable < ActiveRecord::Migration
         english character varying(255),
         is_hidden boolean NOT NULL default false,
         is_landmark boolean NOT NULL default false,
-        classification_id integer REFERENCES classifications(id),
         created_at timestamp without time zone,
         updated_at timestamp without time zone,
         CONSTRAINT pk_categories PRIMARY KEY (id)
       );
       CREATE INDEX idx_categories_is_hidden ON categories(is_hidden);
       CREATE INDEX idx_categories_is_landmark ON categories(is_landmark);
-      CREATE INDEX idx_categories_classification_id ON categories(classification_id);
     }
   end
 
