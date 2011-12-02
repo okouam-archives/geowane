e3b35ac05b35cf491fd69d4819f47cc02e23d6d6
Olivier Kouame <olivier.kouame@gmail.com>
2011-12-02 13:46:56 +0000

    Added PartnerCategory relationship to Partner

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,3 +1,8 @@
 class Partner < ActiveRecord::Base
+  has_many :partner_categories
+
+  def self.dropdown_items
+    Partner.order("name ASC").map {|partner| [partner.name, partner.id]}
+	end
 
 end
