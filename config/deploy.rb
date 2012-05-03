e98147874ed33a96bd3b6a85824e1f98ef12bf3e
docteurdiam <diameramoussa@yahoo.fr>
2012-05-03 11:41:53 +0000

    demo environment to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "geocms"
 set :repository,  "git@github.com:0-One/GeoCMS.git"
 set :scm, :git
 set :branch, :master
-set :stages, ["staging", "production"]
+set :stages, ["staging", "production", "demo"]
 
 set :default_stage, "staging"
 set :deploy_via, :remote_cache
@@ -33,7 +33,7 @@ namespace :bundler do
     release_dir = File.join(current_release, '.bundle')
     run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
   end
- 
+
   task :bundle_new_release, :roles => :app do
     bundler.create_symlink
     run "cd #{release_path} && bundle install --without test"
